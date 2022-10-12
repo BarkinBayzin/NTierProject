5218eb6484bf6e910cf30f2051b6ee0aaff4198f
bayzinbarkin <barkin.bayzin@bilgeadamboost.com>
2022-10-12 14:41:53 +0300

    ProductController CRUD Operations added

--- NTier.UI/Areas/Admin/Controllers/ProductController.cs
+++ NTier.UI/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using NTier.Model.Entities;
 using NTier.Service.Option;
+using NTier.UI.Areas.Admin.Models;
+using NTier.UI.Helpers;
 using PagedList;
 using System;
 using System.Collections.Generic;
@@ -24,7 +26,10 @@ namespace NTier.UI.Areas.Admin.Controllers
         public ActionResult List(int page = 1)
         {
             List<Product> model = _productService.GetActives().OrderByDescending(x => x.CreatedDate).ToList();
-
+            //foreach (var item in model)
+            //{
+            //    item.SubCategory = _subCategoryService.GetById(item.SubCategoryID);
+            //}
             return View(model.ToPagedList(page, 10));
         }
 
@@ -32,6 +37,67 @@ namespace NTier.UI.Areas.Admin.Controllers
         {
             List<SubCategory> model = _subCategoryService.GetActives();
             return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product data, HttpPostedFileBase image)
+        {
+            if (data != null)
+            {
+                data.ImagePath = ImageUploader.UploadSingleImage("~/Uploads/", image);
+
+                if (data.ImagePath == "0" || data.ImagePath == "1" || data.ImagePath == "2")
+                    data.ImagePath = "~/Content/Images/TestPhoto.jpg";
+
+                _productService.Add(data);
+                return Redirect("/Admin/Product/List");
+            }
+
+            return View(data);
+        }
+        //Update View'ına gönderilecek ProductUpdateVM oluşturulur.
+        public ActionResult Update(Guid id)
+        {
+            UpdateProductVM model = new UpdateProductVM()
+            {
+                Product = _productService.GetById(id),
+                SubCategories = _subCategoryService.GetActives()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Product data, HttpPostedFileBase image)
+        {
+            data.ImagePath = ImageUploader.UploadSingleImage("~/Uploads/", image);
+
+            if (data.ImagePath == "0" || data.ImagePath == "1" || data.ImagePath == "2")
+            {
+                Product updated = _productService.GetById(data.Id);
+                if (updated.ImagePath == null || updated.ImagePath == "~/Content/Images/TestPhoto.jpg")
+                {
+                    data.ImagePath = "~/Content/Images/TestPhoto.jpg";
+                }
+                else
+                {
+                    data.ImagePath = updated.ImagePath;
+                }
+            }
+            _productService.Update(data);
+            return Redirect("/Admin/Product/List");
+
+        }
+
+        public RedirectResult Delete(Guid id)
+        {
+            _productService.Remove(id);
+            return Redirect("/Admin/Product/List");
+        }
+
+        public ActionResult Details(Guid id)
+        {
+            return View(_productService.GetById(id));
         }
     }
 }
---
+++ NTier.UI/Areas/Admin/Models/UpdateProductVM.cs
@@ -0,0 +1,14 @@
+﻿using NTier.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NTier.UI.Areas.Admin.Models
+{
+    public class UpdateProductVM
+    {
+        public Product Product { get; set; }
+        public List<SubCategory> SubCategories { get; set; }
+    }
+}
